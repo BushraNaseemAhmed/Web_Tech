87261461d21cde3f3128690219ff2e2395996954
BushraNaseemAhmed <alizaansar211@gmail.com>
2024-06-13 12:42:54 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,70 @@
+function a()
+{
+    document.getElementById("pp").src="./p1.png" 
+}
+function b(){
+    document.getElementById("pp").src="./p2.png"
+}
+function c(){
+    document.getElementById("pp").src="./p3.png"
+}
+function d(){
+    document.getElementById("pp").src="./p4.png"
+}
+function e(){
+    document.getElementById("pp").src="./p5.png"
+}
+function f(){
+    document.getElementById("pp").src="./p6.png"
+}
+function g(){
+    document.getElementById("pp").src="./p7.png"
+}
+function h(){
+    document.getElementById("pp").src="./p8.png"
+}
+function i(){
+    document.getElementById("kk").src="./ride2.png"
+}
+function j(){
+document.getElementById("kk").src="./ride3.png"
+}
+function k(){
+document.getElementById("kk").src="./ride4.png"
+}
+function l(){
+document.getElementById("kk").src="./ride5.png"
+}
+function m(){
+    document.getElementById("app").src="./image/app1.png"
+}
+function n(){
+    document.getElementById("app").src="./p1.png"
+}
+function o(){
+    document.getElementById("app").src="./image/app3.png"
+}
+function p(){
+    document.getElementById("app").src=src="./image/app4.png"
+}
+function q(){
+    document.getElementById("app").src=src="./image/app5.png"
+}
+function r(){
+    document.getElementById("app").src=src="./image/apparel.png"
+}
+function s(){
+document.getElementById("acc").src="./image/accesrories.png"
+}
+function t(){
+document.getElementById("acc").src="./image/acc1.png"
+}
+function u(){
+document.getElementById("lo").src="./image/cloth.png"
+}
+function v(){
+document.getElementById("lo").src="./image/lo1.png"
+}
+function w(){
+document.getElementById("lo").src="./image/lo2.png"
+}
