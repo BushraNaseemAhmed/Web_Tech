ae37fc96a2ee0704be3abbc579e0b2bbf42351ee
BushraNaseemAhmed <alizaansar211@gmail.com>
2024-07-06 19:58:23 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,17 @@
+function a()
+{
+    document.getElementById("cc").src="./image/cus_2.png"
+}
+function b()
+{
+    document.getElementById("cc").src="./image/cus_3.png"
+}
+function c(){
+    document.getElementById("cc").src="./image/cus_4.png"
+}
+function d(){
+    document.getElementById("cc").src="./image/cus_5.png"
+}
+function e(){
+    document.getElementById("cc").src="./image/cus_6.png"
+}
